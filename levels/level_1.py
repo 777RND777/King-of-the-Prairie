d77cbe684ac9a16436ba8e70d64febf205b65148
Nurmukhanbet Rakhimbayev <rakhimbayev.n@tntsecure.kz>
2020-04-06 01:31:19 +0600

    walls were moved to original places

--- levels/level_1.py
+++ levels/level_1.py
@@ -3,13 +3,14 @@ from consts import *
 
 def create_walls():
     level = []
-    for i in range(11):
+    for i in range(6):
         level.append(Wall(SIZE / 2, (i + 0.5) * SIZE))
+        level.append(Wall(SIZE / 2, (i + 9 + 0.5) * SIZE))
         level.append(Wall(WIDTH - SIZE / 2, (i + 0.5) * SIZE))
-    for i in range(1, 14):
-        level.append(Wall((i + 0.5) * SIZE, SIZE / 2))
-        level.append(Wall((i + 0.5) * SIZE, HEIGHT - SIZE / 2))
+        level.append(Wall(WIDTH - SIZE / 2, (i + 9 + 0.5) * SIZE))
     for i in range(1, 7):
-        for j in range(1, 5):
-            level.append(Wall(2 * i * SIZE + SIZE / 2, 2 * j * SIZE + SIZE / 2))
+        level.append(Wall((i + 0.5) * SIZE, SIZE / 2))
+        level.append(Wall((i + 9 + 0.5) * SIZE, SIZE / 2))
+        level.append(Wall((i + 0.5) * SIZE, HEIGHT - SIZE / 2))
+        level.append(Wall((i + 9 + 0.5) * SIZE, HEIGHT - SIZE / 2))
     return level
